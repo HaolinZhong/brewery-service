88e0900659db810afe9e6cbcfa04593b5bc21807
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-02 19:53:44 -0400

    init project

---
+++ src/test/java/hz/spring/breweryservice/BreweryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hz.spring.breweryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
