b96144b02a3c1b05b9ff6b2f3aecc07a1b10d98f
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-03 17:15:38 -0400

    added beer repository

---
+++ src/main/java/hz/spring/breweryservice/repository/BeerRepository.java
@@ -0,0 +1,9 @@
+package hz.spring.breweryservice.repository;
+
+import hz.spring.breweryservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
