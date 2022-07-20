a124a0013f1970cba230faffa9bbc49dcfc567d1
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-19 20:57:43 -0400

    add service implemented with OpenFeign

---
+++ src/main/java/hz/spring/breweryservice/service/inventory/InventoryServiceFeignClient.java
@@ -0,0 +1,19 @@
+package hz.spring.breweryservice.service.inventory;
+
+import hz.spring.breweryservice.service.inventory.model.BeerInventoryDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.UUID;
+
+@FeignClient(name = "brewery-inventory-service")
+public interface InventoryServiceFeignClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
+    ResponseEntity<List<BeerInventoryDTO>> getOnhandInventory(@PathVariable UUID beerId);
+
+}
